e61fda40f9aae0b0aa6fd7a5707b0f0f06956631
Mukhtar Otarbayev <mukhtar.otarbayev@gmail.com>
2020-08-25 11:30:21 +0900

    Added templates using Jinja, created layout, main html page. Started working on forms.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ### This is polyglootal week
+
+## Template
+
+Jinja is a modern and designer-friendly templating language for Python, modelled after Django’s templates. It is fast, widely used and secure with the optional sandboxed template execution environment:
+
+<title>{% block title %}{% endblock %}</title>
+<ul>
+{% for user in users %}
+  <li><a href="{{ user.url }}">{{ user.username }}</a></li>
+{% endfor %}
+</ul>
+
+Jinja uses a template inheritance
+
+### Form Validation with WTForms
+
+pip install flask-wtf
--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Mukhtar Otarbayev',
+        'title': 'First Post',
+        'content': "WBD247 - Has the Bitcoin Bull Woken? With Anthony Pompliano",
+        'date_posted': 'May 23, 2020'
+    },
+    {
+        'author': 'Adam Doe',
+        'title': 'Second Post',
+        'content': "New content",
+        'date_posted': 'May 25, 2020'
+    }
+]
 
-@app.route('/')
-@app.route('/home')
+
+@ app.route('/')
+@ app.route('/home')
 def home():
-    return '<h1>Home Page!</h1>'
+    return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@ app.route('/about')
 def about():
-    return '<h1>About Page!</h1>'
+    return render_template('about.html', title='About')
 
 
 if (__name__) == '__main__':
