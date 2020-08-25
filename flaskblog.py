fde6e161dfbf0d5d19b9922dc17d98930e08db0a
Mukhtar Otarbayev <mukhtar.otarbayev@gmail.com>
2020-08-25 16:01:31 +0900

    Created registration and login forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -35,13 +35,21 @@ def about():
 @ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
-@ app.route('/login')
+@ app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = RegistrationForm()
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@com.com' and form.password.data == 'pass':
+            flash('You logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Please, check your email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
